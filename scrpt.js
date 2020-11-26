4958f539431579dc37b3c9f23b57c91494018c36
Scrirock <73163680+Scrirock@users.noreply.github.com>
2020-11-26 13:59:58 +0100

    bruit

---
+++ scrpt.js
@@ -0,0 +1,29 @@
+function rouge(){
+    let rouge = new Audio();
+    rouge.src = "rouge.wav";
+    rouge.play();
+}
+
+function bleu(){
+    let bleu = new Audio();
+    bleu.src = "bleu.wav";
+    bleu.play();
+}
+
+function jaune(){
+    let jaune = new Audio();
+    jaune.src = "jaune.wav";
+    jaune.play();
+}
+
+function vert(){
+    let vert = new Audio();
+    vert.src = "vert.wav";
+    vert.play();
+}
+
+function mauve(){
+    let mauve = new Audio();
+    mauve.src = "mauve.wav";
+    mauve.play();
+}
